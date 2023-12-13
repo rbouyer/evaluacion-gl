4af57ff18d2084f2f3ee67fde9c2df9bffa94468
rbouyer <rbouyer@akasoft.cl>
2023-12-13 16:55:30 -0300

    Ver.0.0.1 Se configura estructura de proyecto.

---
+++ com.globallogic.evaluacion/src/main/java/model/Phone.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Phone {
+
+		private String id;
+		
+		private Long number;
+		
+		private Integer citycode;
+		
+		private String contrycode;
+		
+		
+		
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public Long getNumber() {
+			return number;
+		}
+		public void setNumber(Long number) {
+			this.number = number;
+		}
+		public Integer getCitycode() {
+			return citycode;
+		}
+		public void setCitycode(Integer citycode) {
+			this.citycode = citycode;
+		}
+		public String getContrycode() {
+			return contrycode;
+		}
+		public void setContrycode(String contrycode) {
+			this.contrycode = contrycode;
+		}
+		
+		
+}
---
+++ com.globallogic.evaluacion/src/main/java/model/User.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class User {
+
+	private String id;
+
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private List<Phone> phones;
+	
+	private String token;
+	
+	private LocalDateTime created;
+	
+	private LocalDateTime lastLogin;
+	
+	private Boolean isActive;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Phone> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(List<Phone> phones) {
+		this.phones = phones;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(LocalDateTime lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	
+	
+}
